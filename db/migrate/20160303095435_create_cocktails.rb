71596a8a818978c53b32cf5ce73b185c3e6fb4f4
WouterKompier <wouterjckompier@gmail.com>
2016-03-03 11:49:11 +0100

    added form

--- db/migrate/20160303095435_create_cocktails.rb
+++ db/migrate/20160303095435_create_cocktails.rb
@@ -1,6 +1,7 @@
 class CreateCocktails < ActiveRecord::Migration
   def change
     create_table :cocktails do |t|
+      t.string :name
 
       t.timestamps null: false
     end
--- db/migrate/20160303095539_create_doses.rb
+++ db/migrate/20160303095539_create_doses.rb
@@ -1,8 +1,11 @@
 class CreateDoses < ActiveRecord::Migration
   def change
     create_table :doses do |t|
-
-      t.timestamps null: false
+    t.integer :ingredient_id
+    t.integer :cocktail_id
+    t.string :description
+      
+    t.timestamps null: false
     end
   end
 end
